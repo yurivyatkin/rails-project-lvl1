e5dca9b0d773895d78d79ab7557f8c069833576c
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2022-12-19 01:09:09 +1300

    Implement div tag and distinguish paired
    TDD

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,9 +6,19 @@ module HexletCode
   class Error < StandardError; end
   # Your code goes here...
 
+  UNPARED_ELEMENTS = %w[br hr img].freeze
+
+  PARED_ELENENTS = %w[div span p h1 h2 h3 h4 h5 h6].freeze
+
   class Tag
     def self.build(tag)
-      "<#{tag}>"
+      normalized_tag = tag.downcase
+
+      return "<#{normalized_tag}>" if UNPARED_ELEMENTS.include?(normalized_tag)
+
+      return "<#{normalized_tag}></#{normalized_tag}>" if PARED_ELENENTS.include?(normalized_tag)
+
+      raise 'Unknown tag'
     end
   end
 end
--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -7,7 +7,18 @@ class HexletCodeTest < Minitest::Test
     refute_nil ::HexletCode::VERSION
   end
 
+  def test_it_raises_given_unknown_tag
+    assert_raises RuntimeError do
+      HexletCode::Tag.build('qq')
+    end
+  end
+
   def test_it_builds_tag_br
-    assert HexletCode::Tag.build('br') == '<br>'
+    assert_equal('<br>', HexletCode::Tag.build('br'))
+  end
+
+  def test_it_builds_tag_div
+    assert_equal('<div></div>', HexletCode::Tag.build('div'))
+    assert_equal('<div></div>', HexletCode::Tag.build('DIV'))
   end
 end
