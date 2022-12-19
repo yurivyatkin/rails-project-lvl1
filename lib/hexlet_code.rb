fd9e5de9cb6952e6dd52c516d51108448202201e
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2022-12-19 23:11:44 +1300

    Implement tags with attributes TDD

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,19 +6,26 @@ module HexletCode
   class Error < StandardError; end
   # Your code goes here...
 
-  UNPARED_ELEMENTS = %w[br hr img].freeze
+  UNPARED_ELEMENTS = %w[br hr img input].freeze
 
   PARED_ELENENTS = %w[div span p h1 h2 h3 h4 h5 h6].freeze
 
   class Tag
-    def self.build(tag)
+    def self.build(tag, attributes = {})
       normalized_tag = tag.downcase
+      attributes_string = hash_to_string(attributes)
 
-      return "<#{normalized_tag}>" if UNPARED_ELEMENTS.include?(normalized_tag)
+      return "<#{normalized_tag}#{attributes_string}>" if UNPARED_ELEMENTS.include?(normalized_tag)
 
-      return "<#{normalized_tag}></#{normalized_tag}>" if PARED_ELENENTS.include?(normalized_tag)
+      return "<#{normalized_tag}#{attributes_string}></#{normalized_tag}>" if PARED_ELENENTS.include?(normalized_tag)
 
       raise 'Unknown tag'
+    end
+
+    def self.hash_to_string(hash)
+      result = ''.dup
+      hash.each_pair { |k, v| result << " #{k}=\"#{v}\"" }
+      result
     end
   end
 end
--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -7,6 +7,13 @@ class HexletCodeTest < Minitest::Test
     refute_nil ::HexletCode::VERSION
   end
 
+  def test_hash_to_string
+    assert_equal(
+      ' type="submit" value="Save"',
+      HexletCode::Tag.hash_to_string({ type: 'submit', value: 'Save' })
+    )
+  end
+
   def test_it_raises_given_unknown_tag
     assert_raises RuntimeError do
       HexletCode::Tag.build('qq')
@@ -20,5 +27,16 @@ class HexletCodeTest < Minitest::Test
   def test_it_builds_tag_div
     assert_equal('<div></div>', HexletCode::Tag.build('div'))
     assert_equal('<div></div>', HexletCode::Tag.build('DIV'))
+  end
+
+  def test_it_builds_tags_with_attributes
+    assert_equal(
+      '<img src="path/to/image">',
+      HexletCode::Tag.build('img', src: 'path/to/image')
+    )
+    assert_equal(
+      '<input type="submit" value="Save">',
+      HexletCode::Tag.build('input', type: 'submit', value: 'Save')
+    )
   end
 end
