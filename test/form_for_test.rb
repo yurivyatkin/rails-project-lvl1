181c43f9546b001d224d6755dd880bd0b03a9d68
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2022-12-25 20:54:46 +1300

    Refactor to reduce repetition in tests

--- test/form_for_test.rb
+++ test/form_for_test.rb
@@ -3,18 +3,19 @@
 require 'test_helper'
 
 class TagTest < Minitest::Test
-  User = Struct.new(:name, :job, keyword_init: true)
+  User = Struct.new(:name, :job, :gender, keyword_init: true)
+  def setup
+    @user = User.new name: 'rob', job: 'hexlet', gender: 'm'
+  end
 
-  def test_form_for
-    user = User.new name: 'rob'
-    form = HexletCode.form_for user
+  def test_form_for_empty
+    form = HexletCode.form_for @user
 
     assert_equal('<form action="#" method="post"></form>', form)
   end
 
-  def test_form_for_with_url
-    user = User.new name: 'rob'
-    form = HexletCode.form_for user, url: '/users'
+  def test_form_for_empty_with_url
+    form = HexletCode.form_for @user, url: '/users'
 
     assert_equal('<form action="/users" method="post"></form>', form)
   end
