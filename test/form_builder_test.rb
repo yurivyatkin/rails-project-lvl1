f63618ec4faab10eed3d2bc38ed0fbd040d71c1c
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2022-12-27 14:48:00 +1300

    Refactor to provide DRY test data

--- test/form_builder_test.rb
+++ test/form_builder_test.rb
@@ -3,16 +3,19 @@
 require 'test_helper'
 
 class FormBuilderTest < Minitest::Test
+  TestEntity = Struct.new(:name, :description, keyword_init: true)
+  def setup
+    @test_entity = TestEntity.new(name: 'Test', description: 'This is a test!')
+  end
+
   def test_form_builder_raises_when_given_no_entity
     assert_raises ArgumentError do
       HexletCode::FormBuilder.new
     end
   end
 
-  Entity = Struct.new(:name)
   def test_form_builder_has_instance_variable_entity
-    entity = Entity.new
-    form_builder = HexletCode::FormBuilder.new(entity)
+    form_builder = HexletCode::FormBuilder.new(@test_entity)
     assert_includes(form_builder.instance_variables, :@entity)
   end
 end
