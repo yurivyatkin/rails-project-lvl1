2fde866fb78f477a033f470a9b6de5a990c647ed
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2023-05-19 12:28:07 +1200

    Improve naming

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 module HexletCode
-  UNPAIRED_ELEMENTS = %w[br hr img input].freeze
+  UNPAIRED_TAGS = %w[br hr img input].freeze
 
-  PAIRED_ELENENTS = %w[div span p h1 h2 h3 h4 h5 h6 label textarea form].freeze
+  PAIRED_TAGS = %w[div span p h1 h2 h3 h4 h5 h6 label textarea form].freeze
 
   class Tag
     def self.build(control, attributes = {})
       normalized_tag = control.downcase
       attributes_string = hash_to_string(attributes)
 
-      return "<#{normalized_tag}#{attributes_string}>" if UNPAIRED_ELEMENTS.include?(normalized_tag)
+      return "<#{normalized_tag}#{attributes_string}>" if UNPAIRED_TAGS.include?(normalized_tag)
 
       content = block_given? ? yield : ''
-      if PAIRED_ELENENTS.include?(normalized_tag)
+      if PAIRED_TAGS.include?(normalized_tag)
         return "<#{normalized_tag}#{attributes_string}>#{content}</#{normalized_tag}>"
       end
 
