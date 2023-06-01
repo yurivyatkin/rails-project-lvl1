7241d0d6b51f3c5b6d0164f7c3ef5fa16ad32cac
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2023-06-01 22:50:13 +1200

    Remove dummy error class

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -3,7 +3,6 @@
 require_relative 'hexlet_code/version'
 
 module HexletCode
-  class Error < StandardError; end
   autoload :Tag, 'hexlet_code/tag'
   autoload :FormBuilder, 'hexlet_code/form_builder'
   autoload :TextArea, 'hexlet_code/controls/textarea'
