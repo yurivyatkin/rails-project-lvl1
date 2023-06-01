0f4ff2353d3b661add6a5f1d4e97849ac3cbea85
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2023-06-01 22:49:19 +1200

    Return the main method to the root file

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,10 +6,13 @@ module HexletCode
   class Error < StandardError; end
   autoload :Tag, 'hexlet_code/tag'
   autoload :FormBuilder, 'hexlet_code/form_builder'
-  autoload :Form, 'hexlet_code/form_for'
   autoload :TextArea, 'hexlet_code/controls/textarea'
   autoload :TextInput, 'hexlet_code/controls/text_input'
   autoload :SubmitButton, 'hexlet_code/controls/submit_button'
 
-  extend HexletCode::Form
+  def self.form_for(entity, form_options = {})
+    form_builder = FormBuilder.new(entity)
+    yield(form_builder) if block_given?
+    form_builder.build(form_options)
+  end
 end
